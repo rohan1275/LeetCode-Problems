b18cc77c4acf9ebd13f01109d0a48f7de5863f90
Rohan Mishra <mishrarohan1275@gmail.com>
2026-06-25 14:12:48 +0530

    Problem 904

---
+++ 904_sort_an_array.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+    
+};
