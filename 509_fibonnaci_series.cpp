e559bce28aaf547a14444f4ef6e2161f6147feab
Rohan Mishra <mishrarohan1275@gmail.com>
2026-06-29 12:08:17 +0530

    Problem 509

---
+++ 509_fibonnaci_series.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int fib(int n) {
+
+    //     if (n == 0)
+    //         return 0;
+
+    //     if (n == 1)
+    //         return 1;
+
+    //     return fib(n - 1) + fib(n - 2);
+    // }
+
+
+    if (n==0){
+        return 0;
+    }
+    int arr[31];
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (int i=2; i <= n; i++ ){
+        arr[i] = arr[i- 1] + arr[i-2];
+    } 
+    return arr[n];
+
+    }
+};
